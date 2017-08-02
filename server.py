94be63c268b9f073b1af97809f7b3c37bab9ca5f
Hackbright Student <no-reply@hackbrightacademy.com>
2017-08-01 18:01:14 -0700

    Added movie list, movie info, and user info routes. Need to add in logged in user rating.

--- server.py
+++ server.py
@@ -21,8 +21,15 @@ app.jinja_env.undefined = StrictUndefined
 @app.route('/')
 def index():
     """Homepage."""
-    #a = jsonify([1,3])
-    return render_template('homepage.html')
+
+    # if the user_email is stored in the session
+    # toggle log in/log out text
+    if 'user_email' in session:
+        btn_text = "Log Out"
+    else:
+        btn_text = "Log In"
+
+    return render_template('homepage.html', btn_text=btn_text)
 
 
 @app.route('/users')
@@ -31,6 +38,28 @@ def show_users():
 
     users = User.query.all()
     return render_template('user_list.html', users=users)
+
+
+@app.route('/movies')
+def show_movies():
+    """Show list of movies"""
+
+    # get list of all movies, ordered by their title, and offset by 1
+    # bc there is no movie with a 0 id
+    movies = Movie.query.order_by(Movie.title).offset(1).all()
+    return render_template('movies.html', movies=movies)
+
+
+@app.route('/movies/<movie_id>')
+def show_movie_info(movie_id):
+    """Gets info and ratings for a specific movie"""
+
+    movie = Movie.query.filter(Movie.movie_id == movie_id).first()
+    #ratings = movie.ratings
+
+    #movie_url = "movies/" + str(movie_id)
+
+    return render_template('movie-info.html', movie=movie)
 
 
 @app.route('/register', methods=["GET"])
@@ -53,13 +82,6 @@ def register_user():
     user_check = User.query.filter_by(email=email).first()
     if user_check:
         #let us know user exists already
-        print
-        print
-        print
-        print "This user already exists"
-        print
-        print
-        print
         flash('You have already registered. Please log in.')
         return redirect('/login')
     else:
@@ -83,25 +105,41 @@ def show_login():
     return render_template('/login.html')
 
 
+@app.route('/logout')
+def logout_user():
+    """Logs a user out"""
+
+    # clear email from the session
+    session.clear()
+
+    # tell the user they are logged out
+    flash("You have successfully logged out")
+
+    return redirect('/')
+
+
 @app.route('/login', methods=["POST"])
 def handle_login():
     """Check is user is registered"""
 
+    # get email and password info
     email = request.form.get("email")
     password = request.form.get("password")
 
     #check if user is in db
     user_check = User.query.filter_by(email=email).first()
 
-    #if user already exists
+    #if user already exists check the password
     if user_check:
         if user_check.password == password:
             #set flash message telling them login successful
             flash("You have successfully logged in")
             #add user's email to the session
             session['user_email'] = email
-            #send them back to homepage
-            return redirect("/")
+            #get the url for that user's info
+            user_url = "/users/" + str(user_check.user_id)
+            #send them to their own page
+            return redirect(user_url)
         else:
             # tell them the password doesn't match
             flash("That password does not match our records.")
@@ -110,6 +148,22 @@ def handle_login():
     else:
         flash("No user is registered with that email. Please register.")
         return redirect('/register')
+
+
+
+@app.route('/users/<user_id>')
+def load_user_info(user_id):
+    """Load info about a specific user"""
+
+    user = User.query.filter(User.user_id == user_id).first()
+    if user:
+        user_age = user.age
+        user_zip = user.zipcode
+        user_movies = user.ratings
+        return render_template('/user-info.html', user=user)
+    else:
+        return "No user exists with that ID. <a href='/users'>Go back to users list</a>"
+
 
 
 if __name__ == "__main__":
@@ -124,5 +178,5 @@ if __name__ == "__main__":
     DebugToolbarExtension(app)
 
 
-    
+
     app.run(port=5000, host='0.0.0.0')
